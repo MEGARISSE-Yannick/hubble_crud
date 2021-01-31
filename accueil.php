5f6865bd3eacb6b7231fcc6dc95ccb3478c11e8d
MEGARISSE-Yannick <67727389+MEGARISSE-Yannick@users.noreply.github.com>
2021-01-31 16:01:13 +0400

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hubble_NASA</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+    <script src="https://kit.fontawesome.com/a3be9883af.js" crossorigin="anonymous"></script>
+    <!--icons link-->
+</head>
+
+<body>
+    <?php include("navbar.php"); ?>
+    <!--navbar link avec include-->
+    <!-- image en premiere vue -->
+    <section class="hero has-text-centered is-dark">
+        <div class="hero">
+            <p class="title ">
+                Bienvenue </p>
+            <p class="subtitle">
+                Voici une bibliothèque d'image prise par hubble</p>
+        </div>
+
+    </section>
+    <div class=".container.is-max-widescreen	">
+        <div class="card-image">
+            <figure class="image">
+            <a href="photos.php"></a>
+                <img src="https://hubblesite.org/files/live/sites/hubble/files/home/_images/hubble_favorites/image-file-4653?t=tn1600" alt="">
+            </figure>
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="content has-text-centered is-dark">
+            <p>
+                <strong>Hubble_NASA</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+                <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+            </p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
---
+++ photos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Hubble_NASA</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
+  <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+  <script src="https://kit.fontawesome.com/a3be9883af.js" crossorigin="anonymous"></script>
+  <!--icons link-->
+</head>
+
+<body>
+  <?php include("navbar.php"); ?>
+  <section class="hero has-text-centered is-dark">
+    <div class="hero">
+      <p class="title ">
+        Voici une gallery d'images pour vous! </p>
+    </div>
+  </section>
+  <section class="section">
+    <?php
+    $bdd = new PDO(
+      'mysql:host=localhost;dbname=nasa;charset=utf8',
+      'Root',
+      'Simplon974',
+      array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+    );
+    $reponse = $bdd->query('SELECT * FROM hubble');
+    while ($donnees = $reponse->fetch()) {
+      echo '       <section class="section has-text-centered">';
+      echo '        <div class="container is-fluid">';
+      echo '      <div class="columns">';
+      echo '          <div class="column">';
+      echo '<img src="' . $donnees['image'] . '"> <br>';
+
+      echo "            </div>";
+      echo "            </div>";
+      echo "    </section>";
+    }
+    ?>
+
+
+
+</body>
+
+</html>
